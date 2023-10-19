7daf78e13a33f6e4f05f1d3f73ecb528af3837b6
Catarina F. M. Castro <catfmcastro@gmail.com>
2023-10-19 11:18:17 -0300

    commit init ui

---
+++ Codigo/Front/src/components/Footer.jsx
@@ -0,0 +1,120 @@
+"use client";
+// components
+import {
+  Box,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  Flex,
+  Tag,
+  useColorModeValue,
+  background,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+const Logo = () => {
+  return <img width="150rem" src="../src/assets/logo-grande.svg" />;
+};
+
+const ListHeader = ({ children }) => {
+  return (
+    <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+      {children}
+    </Text>
+  );
+};
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <Box
+        bg={"#EBF5F8"}
+        color={"#292929"}
+      >
+        <Container as={Stack} maxW={"6xl"} py={8}>
+          <SimpleGrid columns={{ base: 1, sm: 2, md: 4 }} spacing={8}>
+            <Stack align={"flex-start"}>
+              <ListHeader>O site</ListHeader>
+              <Box as="a" href={"#"}>
+                Overview
+              </Box>
+              <Stack direction={"row"} align={"center"} spacing={2}>
+                <Box as="a" href={"#"}>
+                  Features
+                </Box>
+                {/* <Tag
+                size={'sm'}
+                bg={useColorModeValue('green.300', 'green.800')}
+                ml={2}
+                color={'white'}>
+                New
+              </Tag> */}
+              </Stack>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>A Equipe</ListHeader>
+              <Box as="a" href={"#"}>
+                Sobre Nós
+              </Box>
+              <Box as="a" href={"#"}>
+                Entrar em contato
+              </Box>
+              <Box as="a" href={"#"}>
+                Parceirias
+              </Box>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Legal</ListHeader>
+              <Box as="a" href={"#"}>
+                Cookies Policy
+              </Box>
+              <Box as="a" href={"#"}>
+                Politica de Privacidade
+              </Box>
+              <Box as="a" href={"#"}>
+                Termos de Seviço
+              </Box>
+            </Stack>
+            <Stack align={"flex-start"}>
+              <ListHeader>Siga-nos</ListHeader>
+              <Box as="a" href={"#"}>
+                Twitter
+              </Box>
+              <Box as="a" href={"#"}>
+                Instagram
+              </Box>
+              <Box as="a" href={"#"}>
+                LinkedIn
+              </Box>
+            </Stack>
+          </SimpleGrid>
+        </Container>
+        <Box py={5}>
+          <Flex
+            align={"center"}
+            _before={{
+              content: '""',
+              borderBottom: "1px solid",
+              borderColor: useColorModeValue("gray.300", "gray.700"),
+              flexGrow: 1,
+              mr: 8,
+            }}
+            _after={{
+              content: '""',
+              borderBottom: "1px solid",
+              borderColor: useColorModeValue("gray.300", "gray.700"),
+              flexGrow: 1,
+              ml: 8,
+            }}
+          >
+            <Logo />
+          </Flex>
+          <Text pt={6} fontSize={"sm"} textAlign={"center"}>
+            © 2023 Tracker.ly. All rights reserved
+          </Text>
+        </Box>
+      </Box>
+    </footer>
+  );
+}
---
+++ Codigo/Front/src/components/Login.jsx
@@ -0,0 +1,53 @@
+// routes
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// chakra
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from "@chakra-ui/react";
+
+function Login() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen} variant={"btn1"}>Faça seu Login</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Faça seu Login!</ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+            asperiores praesentium ipsum, molestiae quos nesciunt corporis,
+            aliquam beatae labore tenetur enim, fugiat illum. Reprehenderit
+            eveniet repellendus consectetur accusamus, vitae unde.
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant={"btn1"} mr={3} >
+                <Link to={"/perfil"}>
+                    Pronto!
+                </Link>
+            </Button>
+            <Button variant="ghost" mr={2} onClick={onClose} >
+                <Link> 
+                Não tenho uma conta
+                </Link>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default Login;
