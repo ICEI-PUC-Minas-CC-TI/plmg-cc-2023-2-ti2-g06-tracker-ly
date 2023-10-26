4f96f070cc757971e9ffd7bf8bd067d77a701783
Catarina F. M. Castro <catfmcastro@gmail.com>
2023-10-26 15:19:29 -0300

    rotas finalizadas

--- Codigo/Back/src/main/java/objetos/Usuario.java
+++ Codigo/Back/src/main/java/objetos/Usuario.java
@@ -96,37 +96,8 @@ public class Usuario {
         return this.senha;
     }
 
-<<<<<<< HEAD
-    public void setSenha(String senha) {
-        try {
-            // Crie uma instância do MessageDigest com o algoritmo MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // Converte a senha em bytes
-            byte[] senhaBytes = senha.getBytes();
-
-            // Atualiza o MessageDigest com os bytes da senha
-            md.update(senhaBytes);
-
-            // Gere o hash MD5
-            byte[] digest = md.digest();
-
-            // Converte o hash MD5 em uma representação de string hexadecimal
-            StringBuilder hashString = new StringBuilder();
-            for (byte b : digest) {
-                hashString.append(String.format("%02x", b));
-            }
-
-            // Retorne a representação da senha em MD5 como uma string
-            this.senha = hashString.toString();
-        }
-        catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-=======
     public void setSenha(String pass) {
         this.senha = toMD5(pass);
->>>>>>> 58dbc7d6039c827498479069d4ae41ec4b6ac45f
     }
 
     public static String toMD5(String password) {
