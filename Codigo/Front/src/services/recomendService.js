ac0c09f421f8a8d39410bd823d73bd5f6fc18884
Catarina F. M. Castro <catfmcastro@gmail.com>
2023-11-24 09:03:56 -0300

    fix api key

--- Codigo/Front/src/services/recomendService.js
+++ Codigo/Front/src/services/recomendService.js
@@ -5,6 +5,6 @@ export const getChatGpt = async (userInput) => {
       `https://api.openai.com/v1/chat/completions`,{model: "gpt-3.5-turbo", messages: [
         { role: "system", content: "Você é um bot" },
         { role: "user", content: userInput },
-      ]},{headers: {"Content-Type": "application/json", 'Authorization': "Bearer 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"}}
+      ]},{headers: {"Content-Type": "application/json", 'Authorization': "Bearer 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"}}
     );
 };
